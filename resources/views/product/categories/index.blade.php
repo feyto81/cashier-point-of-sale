568644b07a32a48eb115070adb04e3382f8ffbcf
Feyto <feyto81@gmail.com>
2021-07-06 18:52:42 +0700

    add

--- resources/views/product/categories/index.blade.php
+++ resources/views/product/categories/index.blade.php
@@ -145,7 +145,7 @@
   <div class="modal-dialog modal-sm">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Supplier Detail</h5>
+        <h5 class="modal-title">Categories Detail</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
